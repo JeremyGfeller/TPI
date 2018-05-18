7ee06b17fa3920f3ba504160cb6543f9a394bb85
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-18 10:22:42 +0200

    Création d'un fichier de fonction

--- site/out.php
+++ site/out.php
@@ -1,36 +1,7 @@
 <?php
-    error_reporting(E_ERROR);
+    require_once('fonction.php');
+    connectDB();    
 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: Origins, Content-Type");
-    
-    //die(json_encode(print_r($_POST,1)));
-    //error_log(print_r($_POST,1));
-    //error_log(print_r($_POST,1));
-
-    // Toutes les infos nécessaires pour la connexion à une base de donnée
-    $hostname = 'localhost';
-    $dbname = 'caveWine';
-    $username = 'root';
-    $password = '';
-
-    // PDO = Persistant Data Object
-    // Entre "" = Connection String
-    $connectionString = "mysql:host=$hostname; dbname=$dbname";
-
-    global $dbh; 
-
-    try
-    {
-        $dbh = new PDO($connectionString, $username, $password);
-        $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $dbh->exec("SET NAMES UTF8");
-    }
-    catch(PDOException $e)
-    {
-        die("Erreur de connexion au serveur (".$e->getMessage().")");
-    }
-     
     $id_wine = $_POST['idWine'];
     $quantity = $_POST['quantity'];
     $pseudo = $_POST['pseudo'];
@@ -41,5 +12,6 @@
 
     $query = "UPDATE vintage SET quantity = quantity - $quantity WHERE id_vintage = $id_wine;";
     $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-    
+
+    echo json_encode("Les bouteilles ont été retirée !");
 ?>
--- site/stock.php
+++ site/stock.php
@@ -1,47 +1,24 @@
 <?php
+    require_once('fonction.php');
+    connectDB();   
 
-// Toutes les infos nécessaires pour la connexion à une base de donnée
-$hostname = 'localhost';
-$dbname = 'caveWine';
-$username = 'root';
-$password = '';
+    extract($_GET);
 
-// PDO = Persistant Data Object
-// Entre "" = Connection String
-$connectionString = "mysql:host=$hostname; dbname=$dbname";
+    /* Search in the base the data for an article */
+    $query = "select id_vintage, id_wine, name, provider, year, qr_code, quantity, price, date 
+                from vintage 
+                inner join wine on fk_wine = id_wine
+                where id_vintage = $id;";
+                
+    $wines = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
 
-global $dbh; 
+    /* Save the data in a array */
+    while($wine = $wines->fetch()) //fetch = aller chercher
+    {
+        extract($wine); // $id_vintage, $id_wine, $name, $provider, $year, $qr_code, $quantity, $price, $date
+        
+        $arr = array('id_wine' => $id_wine, 'name'=> $name, 'year' => $year, 'quantity' => $quantity);
 
-try
-{
-    $dbh = new PDO($connectionString, $username, $password);
-    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $dbh->exec("SET NAMES UTF8");
-}
-catch(PDOException $e)
-{
-    die("Erreur de connexion au serveur (".$e->getMessage().")");
-}
-
-header("Access-Control-Allow-Origin: *");
-extract($_GET);
-
-/* Search in the base the data for an article */
-$query = "select id_vintage, id_wine, name, provider, year, qr_code, quantity, price, date 
-            from vintage 
-            inner join wine on fk_wine = id_wine
-            where id_vintage = $id;";
-            
-$wines = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-
-/* Save the data in a array */
-while($wine = $wines->fetch()) //fetch = aller chercher
-{
-    extract($wine); // $id_vintage, $id_wine, $name, $provider, $year, $qr_code, $quantity, $price, $date
-    
-    $arr = array('id_wine' => $id_wine, 'name'=> $name, 'year' => $year, 'quantity' => $quantity);
-
-    echo json_encode($arr);
-}
-
+        echo json_encode($arr);
+    }
 ?>
