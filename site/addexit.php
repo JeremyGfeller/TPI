f6a55c835c9ff9590a20d4855eba8e72c2b234a2
Jérémy <jeremy.gfeller@cpnv.ch>
2018-05-22 11:38:12 +0200

    Enlever la date l'ajout de la date lors d'un mouvement
    Quand un mouvement est crée la date sera ajouté automatiquement par la base de donées.

--- site/addexit.php
+++ site/addexit.php
@@ -7,30 +7,24 @@
     {
         $id_wine = $_POST['id_wine'];
         $quantity = $_POST['quantity'];
-        $date = date('Y-m-d');
         
-        $query = "INSERT INTO movement (fk_users, fk_vintage, movement_in, date) VALUES ('1', '$id_wine', '$quantity', '$date');";
+        $query = "INSERT INTO movement (fk_users, fk_vintage, movement_in) VALUES ('1', '$id_wine', '$quantity');";
         $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
 
         $query = "UPDATE vintage SET quantity = quantity + $quantity WHERE id_vintage = $id_wine;";
         $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-
-        echo "Les bouteilles ont été ajoutées !";
     }
 
     if(isset($_POST['out']))
     {
         $id_wine = $_POST['id_wine'];
         $quantity = $_POST['quantity'];
-        $date = date('Y-m-d');
         
-        $query = "INSERT INTO movement (fk_users, fk_vintage, movement_out, date) VALUES ('1', '$id_wine', '$quantity', '$date');";
+        $query = "INSERT INTO movement (fk_users, fk_vintage, movement_out) VALUES ('1', '$id_wine', '$quantity');";
         $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
     
         $query = "UPDATE vintage SET quantity = quantity - $quantity WHERE id_vintage = $id_wine;";
         $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-
-        echo "Les bouteilles ont été retirées !";
     }
 ?>
 <script>
