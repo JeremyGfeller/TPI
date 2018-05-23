36b815c348fa706cd27bbd03ad3d63a70b8c5557
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-23 11:20:59 +0200

    Page qui affichera les qr code

---
+++ site/qr_code.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('fonction.php');
+    connectDB();
+
+    $query = "SELECT id_vintage, qr_code, name, year from vintage inner join wine on fk_wine = id_wine order by id_vintage;";
+    $lastVintages = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+    while($lastVintage = $lastVintages->fetch()) //fetch = aller chercher
+    {
+        extract($lastVintage); // $id_vintage, $qr_code, $name, $year
+        echo "<table style='float: left;'>
+                <img src='qr_code/$qr_code-$name-$year.png'>
+                <p>$qr_code-$name-$year.png</p>
+              </table>";
+    }
+?>
