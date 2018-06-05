20d858e18c3018d8f20af9233b2d5f8a9f160bd0
Jérémy <jeremy.gfeller@cpnv.ch>
2018-06-05 09:21:36 +0200

    WIP

--- site/stock.php
+++ site/stock.php
@@ -4,14 +4,14 @@
 
     //SELECT name, typeWine, year, id_vintage FROM wine INNER JOIN vintage on id_wine = fk_wine INNER JOIN typeWine on fk_typeWine = id_typeWine;
 
-    $query = "SELECT name, typeWine, year, id_vintage FROM wine INNER JOIN vintage on id_wine = fk_wine INNER JOIN typeWine on fk_typeWine = id_typeWine;";
+    $query = "SELECT id_wine, name, typeWine, year, id_vintage FROM wine INNER JOIN vintage on id_wine = fk_wine INNER JOIN typeWine on fk_typeWine = id_typeWine;";
     $allWines = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
     
     $ArrayWines = array();
     foreach($allWines as $allWine)
     {
         extract($allWine); // $name, $typeWine, $year, $id_vintage
-        $res = array('name' => $name , 'typeWine' => $typeWine, 'year' => $year);
+        $res = array('id_wine' => $id_wine, 'id_vintage' => $id_vintage, 'name' => $name, 'typeWine' => $typeWine, 'year' => $year);
 
         $query = "SELECT date, fk_vintage, nb_bottles from movement where fk_vintage = $id_vintage and movement_type = 0 order by date DESC Limit 1";
         $lastInventorys = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
@@ -29,11 +29,11 @@
         
                 $res['quantity'] = $nb_bottle;
                 array_push($ArrayWines, $res);
-
-                echo json_encode($ArrayWines); 
             }
         }
     }
+
+    echo json_encode($ArrayWines); 
 
     //SELECT date, fk_vintage, nb_bottles from movement where fk_vintage = $id_vintage and movement_type = 0 order by date DESC Limit 1;
     //SELECT sum(nb_bottles * movement_type) as quantity from movement where fk_vintage = $id_vintage and date > '$date'
